0154c524c8df051897193700fa2595ca129ebf0e
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-09-28 23:24:54 +0900

    Docs : GCD 비동기 처리 방식 예제 코드 작성

--- Networking.playground/Contents.swift
+++ Networking.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
+import Foundation
+import GLKit
 
-var greeting = "Hello, playground"
+
+// queue - Main, Global, Custom
+
+//Main Queue
+DispatchQueue.main.async {
+    //  ex: UI 업데이트
+    let view = UIView()
+    view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+}
+
+// Global - QoS(userInteractive,userIntitiated, default, utility,
+DispatchQueue.global(qos: .userInteractive).async {
+    // 지금 당장 실행되어야 하는
+}
+DispatchQueue.global(qos: .userInitiated).async {
+    // 유저가 값을 기다리고 있는
+}
+
+// default
+DispatchQueue.global(qos: .default).async {
+    DispatchQueue.global().async {
+        //디폴트는 QoS지정 없어도 가능
+    }
+}
+
+DispatchQueue.global(qos: .utility).async {
+    // 시간이 좀 걸리는 작업들. 사용자가 당장 기다리지 않는 것.
+    // 네트워킹, 크기가 큰 파일 불러오기 등
+}
+
+DispatchQueue.global(qos: .background).async {
+    // 사용자에게 지금 당장 인식 시키지 않아도 되는 것들.
+    // 뉴스 데이터 미리 업데이트, 위치 업데이트
+}
+
+
